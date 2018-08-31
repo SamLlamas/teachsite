0e4b9cdb7cfdefe700087ca9846d0a09d5f05154
Sam Llamas <sam.llamas@utexas.edu>
2018-08-30 19:30:52 -0500

    details update

--- client/src/pages/Detail/Detail.js
+++ client/src/pages/Detail/Detail.js
@@ -16,13 +16,43 @@ class Detail extends Component {
     API.getPost(this.props.match.params.id)
       .then(res => this.setState({ post: res.data }))
       .catch(err => this.props.history.push("/login"));
+  }
+
+  yesno = value => {
+    if (value === true) {
+      return "Yes"
+    }
+    else {
+      return "No"
+    }
+  }
+
+  pets = (no, cats, small, big) => {
+    if (no === true) {
+      return <React.Fragment ><ul><li>No pets allowed</li></ul></React.Fragment>
+    }
+    else {
+      let frag1, frag2, frag3
+      if (cats === true) {
+        frag1 = <li>Cats Allowed</li>
+      }
+      if (small === true) {
+        frag2 = <li>Small dogs Allowed</li>
+      }
+      if (big === true) {
+        frag3 = <li>Big dogs Allowed</li>
+      }
+      let fragment = <React.Fragment ><ul>{frag1}{frag2}{frag3}</ul></React.Fragment>
+      return fragment
+    }
 
   }
 
   render() {
+
     return (
       <Container fluid>
-      <br />
+        <br />
         <Row>
           <Col size="md-12">
             <Jumbotron>
@@ -30,18 +60,45 @@ class Detail extends Component {
           </Col>
         </Row>
         <Row>
-        <div className="col"></div>
+          <div className="col"></div>
           <Col size="md-10 md-offset-1">
             <article>
               <h1>{this.state.post.address}</h1>
               <h3>
-              {this.state.post.bedrooms} Bedroom - {this.state.post.bath} bath - {this.state.post.type}; 
+                {this.state.post.bedrooms} Bedroom - {this.state.post.bath} bath - {this.state.post.type};
               </h3>
               <br />
               <p> {this.state.post.description}</p>
             </article>
+            <Row>
+              <Col size="md-6 md-offset-1">
+                <h3>Amenities:</h3>
+                <ul>
+                  <li>A/C: {this.yesno(this.state.post.ac)}</li>
+                  <li>Garage Parking: {this.yesno(this.state.post.garage)}</li>
+                  <li>Off-Street Parking: {this.yesno(this.state.post.offstreet)}</li>
+                  <li>Furnished: {this.yesno(this.state.post.furnished)}</li>
+                  <li>Pool: {this.yesno(this.state.post.pool)}</li>
+                </ul>
+              </Col>
+              <Col size="md-6 md-offset-1">
+                <h3>Pets:</h3>
+                {this.pets(this.state.post.noPet, this.state.post.cat, this.state.post.smallDog, this.state.post.bigDog)}
+
+              </Col>
+            </Row>
+            <Row>
+              <Col size="md-6 md-offset-1">
+                <h3>Ammenties included: {this.state.post.ammenties}</h3>
+              </Col>
+              <Col size="md-6 md-offset-1">
+                <h3>Laundry: {this.state.post.laundry}</h3>
+              </Col>
+            </Row>
           </Col>
+          <div className="col"></div>
         </Row>
+        <br /><br />
         <Row>
           <Col size="md-2">
             <Link to="/">← Back to Posts</Link>
