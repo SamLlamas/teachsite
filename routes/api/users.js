30ff826dba8a95395c312cdcc4300cdc0f4a5c13
Sam Llamas <sam.llamas@utexas.edu>
2018-10-24 16:18:19 -0500

    testing reset

--- routes/api/users.js
+++ routes/api/users.js
@@ -67,10 +67,7 @@ router.post("/forgot", function (req, res, next) {
                 to: user.email,
                 from: 'resetPass@pantherden.com',
                 subject: 'Password Reset',
-                text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-                    'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-                    'http://' + req.headers.host + '/reset/' + token + '\n\n' +
-                    'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+                text: 'test'
             };
             sgMail.send(msg);
             return res.json(true);
